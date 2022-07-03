fee2dcf014cdaef1a8104411465b0a2feb7b17b7
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-07-03 01:49:48 -0700

    Write user and relationship tests

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationship = Relationship.new(follower_id: users(:user1).id, followed_id: users(:user2).id)
+  end
+
+  test "follower_id is required" do
+    @relationship.follower_id = nil
+
+    assert_not @relationship.valid?
+  end
+
+  test "followed_id is required" do
+    @relationship.followed_id = nil
+
+    assert_not @relationship.valid?
+  end
+  
+  test "user can follow other users" do
+    relationship = relationships(:relationship1)
+    user = users(:user1)
+    second_user = users(:user2)
+    
+    assert user.following?(second_user)
+  end
+  
+  test "user can unfollow other users" do
+    relationship = relationships(:relationship1)
+    user = users(:user1)
+    second_user = users(:user2)
+
+    relationship.destroy
+
+    assert_not user.following?(second_user)
+  end
+
+  test "user cannot follow their own account" do
+    user = users(:user1)
+
+    Relationship.new(follower_id: user.id, followed_id: user.id)
+    assert_not user.following?(user)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,98 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:user1)
+  end
+
+  test "successfully creates new user" do
+    assert @user.valid?
+  end
+
+  test "joins first name and last name" do
+    assert_equal(@user.fullname, "First User")
+  end
+
+  test "will not save user with duplicate username" do
+    @user.save
+
+    new_user = @user.dup
+    new_user.email = "testuser@gmail.com"
+
+    assert_not new_user.valid?
+  end
+
+  test "will not save user with duplicate email" do
+    @user.save
+
+    new_user = @user.dup
+    new_user.username = "testuser"
+
+    assert_not new_user.valid?
+  end
+
+  test "will not save user without a first name" do
+    @user.save
+
+    new_user = @user.dup
+    new_user.first_name = " "
+    new_user.username = "testuser"
+    new_user.email = "testuser@gmail.com"
+
+    assert_not new_user.valid?
+  end
+
+  test "will not save user without a last name" do
+    @user.save
+
+    new_user = @user.dup
+    new_user.last_name = " "
+    new_user.username = "testuser"
+    new_user.email = "testuser@gmail.com"
+
+    assert_not new_user.valid?
+  end
+
+  test "will not save user without a username" do
+    @user.save
+
+    new_user = @user.dup
+    new_user.username = " "
+    new_user.email = "testuser@gmail.com"
+
+    assert_not new_user.valid?
+  end
+
+  test "will not save user without an email address" do
+    @user.save
+
+    new_user = @user.dup
+    new_user.username = "testuser"
+    new_user.email = " "
+
+    assert_not new_user.valid?
+  end
+
+  test "will not save user without a password" do
+    @user.save
+    
+    new_user = @user.dup
+    new_user.username = "testuser"
+    new_user.email = "testuser@gmail.com"
+    new_user.encrypted_password = " "
+
+    assert_not new_user.valid?
+  end
+
+  test "will not save if admin field is empty" do
+    @user.save
+
+    new_user = @user.dup
+    new_user.username = "testuser"
+    new_user.email = "testuser@gmail.com"
+    new_user.admin = nil
+
+    assert_not new_user.valid?
+  end
 end
