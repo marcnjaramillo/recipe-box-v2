97062b66ecf593454269e697b70d2af3017507bc
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-07-03 03:45:15 -0700

    Create recipe helper methods

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,11 @@
 module RecipesHelper
+  def recipe_image recipe
+    image = recipe.image.present? ? recipe.image : "https://via.placeholder.com/500x500.png?text=No+image+to+display"
+    image_tag(image, class: "img-fluid")
+  end
+
+  def recipe_thumbnail recipe
+    image = recipe.image.present? ? recipe.image : "https://via.placeholder.com/500x500.png?text=No+image+to+display"
+    image_tag(image, class: "recipe-thumbnail img-fluid img-thumbnail")
+  end
 end
