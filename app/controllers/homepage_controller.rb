61d3fc77608f8c3f0884189fe0701b9da5e72ae4
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-07-03 01:21:16 -0700

    Rename static controller and view to homepage

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,5 @@
+class HomepageController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+end
--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticController < ApplicationController
-  def homepage
-    
-  end
-end
