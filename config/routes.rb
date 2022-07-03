da0b2e6411a45aa720a3aa467a3fea0a3714e4a3
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-07-03 01:44:31 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  resources :relationships
   resources :likes
   resources :comments
-  resources :recipe_ingredients
-  resources :ingredients
   resources :recipes
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :profile, param: :username, only: [:show] do
+    member do
+      get :recipes, :following, :followers
+    end
+  end
+
+  post 'profile/follow', to: 'profile#follow'
+  delete 'profile/unfollow', to: 'profile#unfollow'
 
   # Defines the root path route ("/")
-  root "static#homepage"
+  root "homepage#index"
 end
