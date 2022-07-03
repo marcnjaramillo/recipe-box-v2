bcaa3b3cf53113255db74264f93b7517dc37e57c
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-07-03 03:44:53 -0700

    Write methods for recipe and ingredient models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_ingredients, inverse_of: :recipe, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
+  has_many :comments, as: :commentable
+  has_one_attached :image
+
+  validates_presence_of :user, :name, :cook_time, :prep_time, :directions, :difficulty, :cuisine, :category
+
+  accepts_nested_attributes_for :recipe_ingredients, reject_if: :all_blank, allow_destroy: true
+
+  def self.categories
+    ['Breakfast', 'Lunch', 'Dinner', 'Desserts', 'Soups', 'Salads', 'Sauces/Dips', 'Appetizers', 'Side Dishes', 'Drinks', 'Snacks']
+  end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,2 +1,15 @@
 class RecipeIngredient < ApplicationRecord
+  belongs_to :recipe, optional: true
+  belongs_to :ingredient, optional: true
+
+  validates :quantity, presence: true
+
+  def ingredient_attributes=(ingredient_attributes)
+    ingredient_attributes.values.each do |attribute|
+      if attribute != ''
+        new_ingredient = Ingredient.find_or_create_by(name: attribute)
+        self.ingredient = new_ingredient
+      end
+    end
+  end
 end
