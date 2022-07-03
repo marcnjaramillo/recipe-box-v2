31d57d52649cf1122ef2fdd498fe5bac75e438c6
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-07-03 01:37:17 -0700

    Write helper methods for profile

---
+++ app/helpers/profile_helper.rb
@@ -0,0 +1,25 @@
+module ProfileHelper
+  include ActionView::RecordIdentifier
+
+  def following?(user)
+    current_user&.following&.include?(user)
+  end
+
+  def dom_id_for_follower(follower)
+    dom_id(follower)
+  end
+
+  def profile_image user
+    image = user.profile_image.present? ? user.profile_image : "default.png"
+    image_tag(image, class: "profile-pic m-r-10")
+  end
+
+  def follow_image user
+    image = user.profile_image.present? ? user.profile_image : "default.png"
+    image_tag(image, class: "follow-pic m-r-10")
+  end
+
+  def can_edit_profile? profile_id
+    user_signed_in? && current_user.id == profile_id
+  end
+end
