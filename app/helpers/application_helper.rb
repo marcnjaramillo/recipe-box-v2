b6e11acbd00432cd00d049a0341d73aac0ab2be9
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-07-05 03:42:12 -0700

    Add new helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
   def is_active(path)       
-    request.path == path ? 'bg-cyan-700 text-white' : 'text-gray-300 hover:bg-cyan-600 hover:text-whit'     
+    request.path == path ? 'bg-cyan-700 text-white' : 'text-gray-300 hover:bg-cyan-600 hover:text-white'     
+  end
+
+  def profile_nav_active(path)       
+    request.path == path ? 'bg-cyan-200' : ''     
   end
 
   def nested_active(controller)       
-    params[:controller] == controller ? 'active' : ''     
+    params[:controller] == controller ? 'bg-cyan-700' : ''     
   end
 end
